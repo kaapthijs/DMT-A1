602640738e4b9e33d70ec04b12b8f151719c1d44
Lars de Wolf <l.de.wolf@student.vu.nl>
2024-04-08 20:37:18 +0200

    features_engineering: - creates features by looking at all of the dates containing records for 'mood' variable (target days) for each id, and sliding 'window_size' amount of days back. - includes mean and aggregated features for each variable - simple evaluation for classification and regression at the end

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from collections import OrderedDict
-import numpy as np
-from math import gcd
+from sklearn import tree
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
 
 
 pd.set_option('future.no_silent_downcasting', True)
@@ -22,26 +23,47 @@ def create_features(df, window_size=10):
 
     X = []
     Y = []
-    variables = df['variable'].drop_duplicates()
 
     for id, data in df.drop(columns=df.columns[0]).sort_values('time').groupby('id'):
+        # We can only compare prediction to days where a mood is recorded
         target_dates = list(OrderedDict.fromkeys([x.normalize() for x in data[data['variable'] == 'mood']['time']]))
 
         for date in target_dates:
             features = {}
-            date_window = date - pd.Timedelta(days=window_size - 1)
-            data_9 = data[(data['time'] >= date_window) & (data['time'] < date)]
-            data_10 = data[(data['time'] >= date) & (data['time'] < date + pd.Timedelta(days=1))]
+            date_window = date - pd.Timedelta(days=window_size)  # window of [target_date - window_size, target_date + 1)
+            data_n_1 = data[(data['time'] >= date_window) & (data['time'] < date)]
+            data_n = data[(data['time'] >= date) & (data['time'] < date + pd.Timedelta(days=1))]
 
-            features['mean_mood'] = 0 if data_9[data_9['variable'] == 'mood'].empty else\
-                data_9[data_9['variable'] == 'mood']['value'].mean()
-            features['calls_sum'] = data_9[data_9['variable'] == 'call']['value'].sum()
-            features['sms_sum'] = data_9[data_9['variable'] == 'call']['value'].sum()
+            score_vars = ['mood', 'circumplex.arousal', 'circumplex.valence']
+            for var in score_vars:
+                # Mean of variable
+                features[f"mean_{var}"] = -10 if data_n_1.loc[data_n_1['variable'] == var].empty else \
+                    data_n_1.loc[data_n_1['variable'] == var]['value'].mean()
+                # Mean of last 3 instances of var
+                features[f"last3_mean_{var}"] = -10 if len(data_n_1.loc[data_n_1['variable'] == var]) < 3 else \
+                    data_n_1.loc[data_n_1['variable'] == var]['value'].iloc[:3].fillna(0).mean()
+
+            for var in [x for x in list(df['variable'].drop_duplicates()) if x not in score_vars]:
+                # Aggregating the variables
+                features[f"aggr_{var}"] = 0 if data_n_1.loc[data_n_1['variable'] == var].empty else\
+                    data_n_1.loc[data_n_1['variable'] == var]['value'].sum()
 
             X.append(features)
-            Y.append(data_10[data_10['variable'] == 'mood']['value'].mean())
+            Y.append(data_n[data_n['variable'] == 'mood']['value'].mean())
+
     return X, Y
 
 
 x, y = create_features(dataset, window_size=10)
 
+x_train, x_test = [list(point.values()) for point in x[200:]], [list(point.values()) for point in x[:200]]
+y_train, y_test = [round(point) for point in y[200:]], [round(point) for point in y[:200]]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x_train, y_train)
+print(accuracy_score(y_test, clf.predict(x_test)))
+
+y_train, y_test = [point for point in y[200:]], [point for point in y[:200]]
+reg = LinearRegression().fit(x_train, y_train)
+print(reg.score(x_train, y_train))
+print(reg.score(x_test, y_test))
